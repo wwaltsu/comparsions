853bbb658589e0770e88b09c417edc9c7b6a8891
Waltsu <waltsumarcus@gmail.com>
2023-05-06 19:21:37 +0300

    Add style to rendered 10 000 divs

--- testPreact/src/components/app.js
+++ testPreact/src/components/app.js
@@ -1,47 +1,17 @@
 import { h } from 'preact'
+import style from './style.css'
 
-/* const DivList = () => {
-  const divList = Array.from({ length: 5000 }, (_, i) => (
-    <div>div numero: {i}</div>
-  ))
-  return <div>{divList}</div>
-} */
-
-/* const DivList = () => {
-	const divList = Array(10000)
-	  .fill()
-	  .map((_, i) => <div>div numero: {i}</div>);
-	return <div>{divList}</div>;
-  }
- */
-/* 
-  function* range(start, end) {
-	for (let i = start; i <= end; i++) {
-	  yield i
-	}
-  }
-  
-  const DivList = () => {
-	const divList = [];
-	for (let i of range(0, 9999)) {
-	  divList.push(<div>div numero: {i}</div>)
-	}
-	return <div>{divList}</div>
-  } */
-
-  
 const DivList = () => {
 	const divList = Array.from({ length: 10000 }, (_, i) => (
-	  <div>div numero: {i}</div>
+		<div class={style['item-box']}>div numero: {i + 1}</div>
 	))
-	return <div>{divList}</div>
-  }
-
+	return <div class={style['container']}>{divList}</div>
+}
 
 const App = () => (
-  <div id="app">
-    <DivList />
-  </div>
+	<div id="app" class={style['app-container']}>
+		<DivList />
+	</div>
 )
 
-export default App;
+export default App
